d2794fed1706842c96271649b2c23dc25fdfbe42
Jim Carucci, Jr <jim.carucci@protonmail.com>
2023-07-29 15:28:52 -0500

    Add template for config.h

---
+++ bloqBox/config_template.h
@@ -0,0 +1,15 @@
+// config.h
+
+//Add your params here and save this as config.h
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi credentials
+const char* ssid = "YOUR WIFI";
+const char* password = "YOUR WIFI PASSWORD";
+
+// API token
+const char* api_token = "YOUR API TOKEN FOR SERVER HERE";
+
+#endif
